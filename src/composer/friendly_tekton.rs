acecc97d4f50fa13f35d33538e964e6590821ec5
Nicholas O'Kelley <okelleynp@gmail.com>
2022-10-12 15:32:14 -0400

    feat: Add improved structure

--- src/composer/friendly_tekton.rs
+++ src/composer/friendly_tekton.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use serde::de::IntoDeserializer;
 
 use crate::{
     errors::TektonError,
@@ -11,7 +12,7 @@ use crate::{
 
 use super::vimsnippet_tekton::gen_snippet;
 
-/// A private helper function to handle `.snippet` -> `.json`
+/// A helper function to handle `.snippet` -> `.json`
 pub fn compose_friendly_snippets(lines: Vec<String>) -> Result<String, TektonError> {
     let snips = gen_snippet(lines);
     let friendlies: Vec<FriendlySnippet> = gen_friendly_snippets(snips);
@@ -82,4 +83,92 @@ pub fn build_friendly_string(friendlies: Vec<FriendlySnippet>) -> Result<String,
     print!("\x1B[2J\x1B[1;1H");
     finished += "\n}";
     Ok(finished)
+}
+
+pub fn sort_friendly_snippets(json_snippets: String) -> Result<String, TektonError> {
+    let json: serde_json::Value = serde_json::from_str(&json_snippets).unwrap();
+    let mut snippets: Vec<FriendlySnippet> = Vec::new();
+    for s in json.into_deserializer().as_object() {
+        for (name, v) in s {
+            let mut body: Vec<String> = Vec::new();
+
+            for line in v["body"].as_array().unwrap().iter() {
+                body.push(line.to_string());
+            }
+
+            let snippet_body = FriendlySnippetBody::new(
+                v["prefix"].to_string(),
+                body,
+                v["description"].to_string(),
+            );
+
+            snippets.push(FriendlySnippet {
+                name: name.to_string(),
+                snip_body: snippet_body,
+            });
+        }
+    }
+
+    // Inplace sort of the snippets by their name
+    snippets.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+
+    let writable = serde_json::to_string(&snippets);
+
+    match writable {
+        Ok(s) => Ok(s),
+        Err(e) => Err(TektonError::Reason(e.to_string())),
+    }
+}
+
+#[test]
+fn test_snippet_alphabatizing() {
+    let mut snippets: Vec<FriendlySnippet> = Vec::new();
+    snippets.push(FriendlySnippet {
+        name: String::from("zebra"),
+        snip_body: FriendlySnippetBody {
+            prefix: "wumbo".to_string(),
+            body: Vec::new(),
+            description: String::new(),
+        },
+    });
+    snippets.push(FriendlySnippet {
+        name: String::from("Plant"),
+        snip_body: FriendlySnippetBody {
+            prefix: "wumbo".to_string(),
+            body: Vec::new(),
+            description: String::new(),
+        },
+    });
+    snippets.push(FriendlySnippet {
+        name: String::from("pLants"),
+        snip_body: FriendlySnippetBody {
+            prefix: "wumbo".to_string(),
+            body: Vec::new(),
+            description: String::new(),
+        },
+    });
+    snippets.push(FriendlySnippet {
+        name: String::from("apples"),
+        snip_body: FriendlySnippetBody {
+            prefix: "wumbo".to_string(),
+            body: Vec::new(),
+            description: String::new(),
+        },
+    });
+    snippets.push(FriendlySnippet {
+        name: String::from("Apples"),
+        snip_body: FriendlySnippetBody {
+            prefix: "wumbo".to_string(),
+            body: Vec::new(),
+            description: String::new(),
+        },
+    });
+
+    snippets.sort_by(|a, b| a.name.to_lowercase().cmp(&b.name.to_lowercase()));
+
+    assert_eq!(snippets.get(0).unwrap().name, "apples".to_string());
+    assert_eq!(snippets.get(1).unwrap().name, "Apples".to_string());
+    assert_eq!(snippets.get(2).unwrap().name, "Plant".to_string());
+    assert_eq!(snippets.get(3).unwrap().name, "pLants".to_string());
+    assert_eq!(snippets.get(4).unwrap().name, "zebra".to_string());
 }
